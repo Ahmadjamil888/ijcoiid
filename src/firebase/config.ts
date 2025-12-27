aa4febc7da3f585d8452d1c7747ecdbca7d04140
Ahmad jamil <ahmad.jamil.888.202020@gmail.com>
2025-12-27 07:24:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9181914318-a6329",
+  "appId": "1:595378890242:web:1ddf36226d219966c6e91b",
+  "apiKey": "AIzaSyBy-efgWTnlh-zylomZBk-kdi-JtDt6hPw",
+  "authDomain": "studio-9181914318-a6329.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "595378890242"
+};
